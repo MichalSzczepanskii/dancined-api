bd18cef8ca2f94f27c7be66dd2944a9f3d1a59c2
Michał Szczepański <szczepanskimichal99@gmail.com>
2022-07-02 15:14:26 +0200

    DAN-6 add permissions for super-admin

--- database/seeders/auth/PermisionSeeder.php
+++ database/seeders/auth/PermisionSeeder.php
@@ -16,17 +16,39 @@ class PermisionSeeder extends Seeder
      */
     public function run()
     {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        // create permissions
+
+        //Users permissions
         Permission::create(['name' => 'users.read_one']);
         Permission::create(['name' => 'users.read_all']);
+        Permission::create(['name' => 'users.create']);
+        Permission::create(['name' => 'users.update']);
+        Permission::create(['name' => 'users.delete']);
 
+        //Locations permissions
         Permission::create(['name' => 'locations.read_one']);
         Permission::create(['name' => 'locations.read_all']);
+        Permission::create(['name' => 'locations.create']);
+        Permission::create(['name' => 'locations.update']);
+        Permission::create(['name' => 'locations.delete']);
 
+
+        //Lesson types permissions
         Permission::create(['name' => 'lesson-types.read_one']);
         Permission::create(['name' => 'lesson-types.read_all']);
+        Permission::create(['name' => 'lesson-types.create']);
+        Permission::create(['name' => 'lesson-types.update']);
+        Permission::create(['name' => 'lesson-types.delete']);
 
+        //Rooms permissions
         Permission::create(['name' => 'rooms.read_one']);
         Permission::create(['name' => 'rooms.read_all']);
+        Permission::create(['name' => 'rooms.create']);
+        Permission::create(['name' => 'rooms.update']);
+        Permission::create(['name' => 'rooms.delete']);
 
         $superAdmin = Role::findByName('super-admin');
         $superAdmin->givePermissionTo(Permission::all());
